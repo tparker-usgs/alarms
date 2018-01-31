a366360bd2b9edf3831dc9f51c9775195c21fccb
Tom Parker <tparker@usgs.gov>
2018-01-31 05:19:15 +0000

    add jinja

--- alarmlogger/alarmlogger/writer.py
+++ alarmlogger/alarmlogger/writer.py
@@ -1,8 +1,21 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
 from .db import Db
+
+
 
 def write():
     conn = Db()
     alarms = conn.get_alarms()
     print("TOMP SAYS", alarms)
+    render(alarms)
  
 
+def render(alarms):
+    env = Environment(
+        loader=PackageLoader('alarmlogger', 'templates'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('alarmlog.html')
+
+    print(template.render(alarms=alarms))
