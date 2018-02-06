587fdcdc37a8883488f59d171bbe14397110f249
Tom Parker <tparker@usgs.gov>
2018-02-06 22:45:34 +0000

    init protobuf

--- alarmlogger/alarmlogger/__main__.py
+++ alarmlogger/alarmlogger/__main__.py
@@ -14,7 +14,7 @@ channel = connection.channel()
 channel.exchange_declare(exchange=exchange, exchange_type='topic')
 result = channel.queue_declare(exclusive=True)
 queue_name = result.method.queue
-channel.queue_bind(exchange=exchange, queue=queue_name, routing_key="#")
+channel.queue_bind(exchange=exchange, queue=queue_name, routing_key="test.key")
 
 print(' [*] Waiting for logs. To exit press CTRL+C')
 channel.basic_consume(callback, queue=queue_name, no_ack=True)
--- alarmlogger/alarmlogger/broker.py
+++ alarmlogger/alarmlogger/broker.py
@@ -4,27 +4,20 @@ Receive messages from broker.
 """
 import json
 
-import alarm_pb2
+from .alarm_pb2 import Alarm
 
 FILENAME_SUFFIX = "_%Y%m%d.pb2"
 
 def callback(ch, method, properties, body):
     print("ROUTING KEY", method.routing_key)
-    alarm = alarm_pb2.Alarm()
-    alarm.ParseFromString(body)
+    alarm = Alarm()
+    #alarm.ParseFromString(body)
 
     now = datetime.now()
     suffix = now.strftime(FILENAME_SUFFIX)
 
     short_name = filter(str.isalnum, alarm.name)
     filename = "{}.{}.{}.{}".format(alarm.type, alarm.name, alarm.state)
-    with open(this_file, 'ab') as f:
-        f.write(alarm.SerializeToString())
+    #with open(filename, 'ab') as f:
+    #    f.write(alarm.SerializeToString())
 
-
-def print_message(alarm):
-    print("New alarm:");
-    print("volcano: {}".format(alarm['volcano']))
-    print("subject: {}".format(alarm['subject']))
-    print("message: {}".format(alarm['message']))
-    print("\n\n");
